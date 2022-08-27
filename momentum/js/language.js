b8d7b3b51ab77523870fd94ee91152e8db5649fa
shamkolovich <shamkolovich95@mail.ru>
2022-08-27 13:12:25 +0300

    init: momentum

---
+++ momentum/js/language.js
@@ -0,0 +1,25 @@
+const lang = {
+  "en": {
+    "language": "en",
+    "greeding": ["Good", "Good", "Good", "Good"],
+    "time": ["morning", "afternoon", "evening", "night"],
+    "wind": "Wind speed",
+    "count": "m/s",
+    "humidity": "Humidity",
+    "inputName": "[Enter name]",
+    "english": "English",
+    "russian": "Russian",
+  },
+  "ru": {
+    "language": "ru",
+    "greeding": ["Доброе", "Доброго", "Добрый", "Доброй"],
+    "time": ["утро", "дня", "вечер", "ночи"],
+    "wind": "Скорость ветра",
+    "count": "м/с",
+    "humidity": "Влажность",
+    "inputName": "[Впишите имя]",
+    "english": "Английский",
+    "russian": "Русский",
+  }
+}
+export default lang;
